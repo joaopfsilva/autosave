ac427330c574f2e8598502d4c7ea1a42fe19a89d
João Pedro Ferreira da Silva <j0a0.silva.kle@gmail.com>
2019-02-12 16:43:05 +0100

    dummy example of autosave

---
+++ app/assets/javascripts/cities.js
@@ -0,0 +1,8 @@
+$( document ).ready(function() {
+	console.log('lwwwwwwwwwww');
+	$("#newCity").change( function(e) {
+		$("#saved_message").fadeIn( 300 ).delay( 2000 ).fadeOut( 300 ); 
+
+		console.log('klhsdkjfhaljfldfldhf');
+	});
+});
